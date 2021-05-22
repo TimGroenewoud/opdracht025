096cbccfae1570d77ddca0043251064c2f88e2d7
Tim Groenewoud <74865860+TimGroenewoud@users.noreply.github.com>
2021-05-22 11:35:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// Defining a Function (Bepalen van de functie)
+
+const makePizza = function () {
+    console.log('Making the dough');
+    console.log('Roll the dough');
+    console.log('Put some tomato sauce on it');
+    console.log('And then some cheese and vegetables');
+    console.log('Put it in the oven for 20 min on 200 degrees');
+};
+
+const makeSushi = function () {
+    console.log('Boil rice');
+    console.log('cut the salmon');
+    console.log('Roll the sushi');
+    console.log('enjoy it')
+};
+
+// Calling a Function (Aanzetten of uitvoeren van de functie)
+
+makeSushi();
+makePizza();
+makeSushi();
+makePizza();
+makeSushi();
+
